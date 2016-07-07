bb59e1dbb67fb1576494f0488b01068f5cf12b5d
David Smith <getdavemail@gmail.com>
2016-07-07 22:14:40 +0100

    Add initial test for global helper. Add dummy test image

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -9,10 +9,13 @@ namespace WPIntervention;
 
 use phpmock\phpunit\PHPMock;
  
-class DemoTest extends \PHPUnit_Framework_TestCase
+class CoreTest extends \PHPUnit_Framework_TestCase
 {
     use PHPMock;
  
-    // ...
+    public function test_global_function()
+    {
+        $this->assertTrue( function_exists( 'wp_intervention' ), 'Global helper function defined' );
+    }
 }
 
