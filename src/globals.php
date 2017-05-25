80af4a3496b19d1019122c51c7f8eb0285111135
David Smith <david.smith@mrbandfriends.co.uk>
2017-05-25 09:41:45 +0100

    Fix major performance issue. Enable ability to return Intervention instance
    Fixes major issue with caching logic whereby the Intervention lib was being initialised even if file was in cache. Reverted to using raw PHP and significantly improved performance. Also introduced ability to return underlying Intervention.io instance for non-cached requests.

--- src/globals.php
+++ src/globals.php
@@ -4,9 +4,9 @@
  * WP INTERVENTION GLOBAL HELPER
  */
 if ( !function_exists('wp_intervention') ) {
-	function wp_intervention( $src=null, $intervention_args=array(), $plugin_args=array() ) {
+	function wp_intervention( $src=null, $intervention_args=array(), $plugin_args=array(), $return_instance=false ) {
 		$wp_intervention = new Intervention_Wrapper( $src, $intervention_args, $plugin_args ); 
-		return $wp_intervention->process();
+		return $wp_intervention->process($return_instance);
 	}	
 }
 
